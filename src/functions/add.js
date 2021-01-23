9f4bae788fd82abc7f9df59ec70cf081f3e4b279
Darkfire126 <63691497+Darkfire126@users.noreply.github.com>
2021-01-22 18:28:23 -0800

    Update

---
+++ src/functions/add.js
@@ -0,0 +1,7 @@
+const add = (num1, num2) => {
+    if(!num1) return TypeError("Please use 1,2 <Example> format")
+    if(!num2) return TypeError("Please use 1,2 <Example> format")
+    return +num1 + +num2
+}
+
+module.exports.add = add
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-const add = (num1, num2) => {
-    if(!num1) return TypeError("Please use 1,2 <Example> format")
-    if(!num2) return TypeError("Please use 1,2 <Example> format")
-    return +num1 + +num2
-}
 
-module.exports.add = add
-console.log(add(1,2))
+const Discord = require("discord.js")
+require('dotenv').config()
+const client = new Discord.Client()
+const token = process.env.TOKEN
+const prefix = process.env.PREFIX
+if(!token) return TypeError("Please make a .env file and put TOKEN=<insert your token>")
+if(!prefix) return TypeError("Please make a .env file and put PREFIX=<insert your prefix>")
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
+const fs = require("fs")
+fs.readdir("./commands", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[CMDS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        client.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            client.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+client.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(client,message,args)
+
+})
